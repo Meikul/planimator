9455ff3482e034e2af44a7f788f4d2853f468f8c
Meikul <meikul.harris@gmail.com>
2017-07-10 12:38:09 -0600

    starting keyframe reorder

--- planimator.js
+++ planimator.js
@@ -8,12 +8,24 @@ window.onload = init;
 function init(){
   var body = document.getElementsByTagName('body');
   var draggiePanel = new Draggabilly('#pl-panel', {handle:'.pl-panel-header', containment:true});
-  // var draggieHead = new Draggabilly('.pl-frame', {handle:'.pl-header', containment:true, axis:'y'});
+  forAll('.pl-frame', function(elem){
+    var draggieHead = new Draggabilly(elem, {handle:'.pl-header .fa-bars', containment:true, axis:'y'});
+
+    draggieHead.on('pointerDown', keyReorder);
+    draggieHead.on('dragMove', keyReorder);
+    draggieHead.on('pointerUp', keySettle);
+  });
+
   // document.querySelectorAll('.pl-plus').forEach(function(elem){
   //   elem.onclick = toggleDrop;
   // });
   // forAll('.pl-header', function(elem){
   //   elem.onclick = reorder;
+  // });
+  // document.addEventListener('mousemove', drag);
+  // forAll('.pl-draggable', function(elem){
+  //   elem.onmousedown = function(){ this.classList.add('pl-dragging'); };
+  //   elem.onmouseup = function(){ this.classList.remove('pl-dragging'); };
   // });
   forAll('.pl-header-buttons .fa-plus', function(elem){
     elem.onclick = toggleDrop;
@@ -28,6 +40,39 @@ function init(){
   });
   forAll('.pl-frame-title input', function(elem){
     elem.oninput = changeTitle;
+  });
+}
+
+function keyReorder() {
+  var header = this.element;
+  header.classList.add('.pl-header-dragging');
+  // this.dragStartPoint.y % height
+  console.log(this);
+}
+
+function keySettle() {
+
+}
+
+function drag(e) {
+  forAll('.pl-dragging', function(e){
+    if (drag.lastX === undefined){
+      drag.lastX = e.clientX;
+      drag.lastY = e.clientY;
+      var panel = document.querySelector('#pl-panel');
+      drag.lastLeft = panel.style.left = panel.getBoundingClientRect().left+'px';
+      drag.lastTop = panel.style.top = panel.getBoundingClientRect().top+'px';
+      console.log(drag.lastLeft, drag.lastTop);
+      return;
+    }
+    var deltaX = e.clientX - drag.lastX;
+    var deltaY = e.clientY - drag.lastY;
+    var panel = document.querySelector('#pl-panel');
+    console.log(panel.style.left, panel.style.top);
+    panel.style.left = parseInt(panel.style.left)+deltaX+'px';
+    panel.style.top = parseInt(panel.style.top)+deltaY+'px';
+    drag.lastX = e.clientX;
+    drag.lastY = e.clientY;
   });
 }
 
